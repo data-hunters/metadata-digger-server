860aa039f79ed5c033fe8d0fd8e6fcc55faa8be9
Michał Gutowski <michal@cucumbersome.net>
2019-11-06 21:42:04 +0100

    Cors added

--- src/main/java/ai/datahunters/md/server/photos/http/PhotosController.java
+++ src/main/java/ai/datahunters/md/server/photos/http/PhotosController.java
@@ -4,6 +4,7 @@ import ai.datahunters.md.server.photos.SearchRequest;
 import ai.datahunters.md.server.photos.http.json.SearchResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,7 @@ public class PhotosController {
         this.handler = handler;
     }
 
+    @CrossOrigin(origins = "*")
     @PostMapping(value = "/api/v1/photos", consumes = MediaType.APPLICATION_JSON_VALUE)
     public Mono<SearchResponse> getPhotos(@RequestBody SearchRequest request) {
         return handler.search(request);
