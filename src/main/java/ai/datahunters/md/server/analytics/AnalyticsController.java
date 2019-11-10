f8049b7b2bc1d7d6b962136f145ff47935d56dd0
NotNor <fumax@wp.pl>
2019-11-10 15:11:47 +0000

    minor changes

--- src/main/java/ai/datahunters/md/server/analytics/AnalyticsController.java
+++ src/main/java/ai/datahunters/md/server/analytics/AnalyticsController.java
@@ -2,11 +2,10 @@ package ai.datahunters.md.server.analytics;
 
 import lombok.AllArgsConstructor;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
-
-import java.util.concurrent.ExecutionException;
 
 @AllArgsConstructor
 @RestController
@@ -14,8 +13,9 @@ public class AnalyticsController {
 
     private AnalyticsHandler handler;
 
+    @CrossOrigin(origins = "*")
     @GetMapping(value = "/api/v1/analytics", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Mono<AnalyticsData> getAnalytics() throws ExecutionException, InterruptedException {
+    public Mono<AnalyticsData> getAnalytics() {
         return handler.getAnalytics();
     }
 
