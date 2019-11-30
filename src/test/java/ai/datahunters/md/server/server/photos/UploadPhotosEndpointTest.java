c5319ee3c89d0bfe5e2c5111a02f728bde93fe01
NotNor <49325657+NotNor@users.noreply.github.com>
2019-11-30 16:03:39 +0100

    Update UploadPhotosEndpointTest.java

--- src/test/java/ai/datahunters/md/server/server/photos/UploadPhotosEndpointTest.java
+++ src/test/java/ai/datahunters/md/server/server/photos/UploadPhotosEndpointTest.java
@@ -51,4 +51,6 @@ public class UploadPhotosEndpointTest {
                 .consumeWith(respBody ->
                         verifyJsonOutput(respBody.getResponseBody(), expectedResponse)
                 );
+
+    }
 }
