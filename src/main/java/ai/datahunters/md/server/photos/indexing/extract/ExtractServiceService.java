9827b7beaa5ef618bd2a6b504cf02f399b035fe3
Michał Gutowski <michal@cucumbersome.net>
2020-01-02 13:33:14 +0100

    Use lombok logging

--- src/main/java/ai/datahunters/md/server/photos/indexing/extract/ExtractServiceService.java
+++ src/main/java/ai/datahunters/md/server/photos/indexing/extract/ExtractServiceService.java
@@ -3,8 +3,7 @@ package ai.datahunters.md.server.photos.indexing.extract;
 import ai.datahunters.md.server.photos.indexing.filesystem.FileService;
 import ai.datahunters.md.server.photos.indexing.upload.FileUploaded;
 import ai.datahunters.md.server.photos.indexing.uploadid.UploadId;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
@@ -14,11 +13,10 @@ import java.util.Arrays;
 import java.util.List;
 
 @Component
+@Slf4j
 public class ExtractServiceService {
     private ArchiveHandler archiveHandler;
     private FileService fileService;
-
-    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     public ExtractServiceService(ArchiveHandler archiveHandler, FileService fileService) {
         this.archiveHandler = archiveHandler;
@@ -26,11 +24,11 @@ public class ExtractServiceService {
     }
 
     public void unarchiveUploadedFile(FileUploaded fileUploaded) {
-        logger.info("Extraction started for upload id" + fileUploaded.getUploadId());
+        log.info("Extraction started for upload id" + fileUploaded.getUploadId());
         openFile(fileUploaded.getUploadedFilePath())
                 .flatMap(is -> this.handleUnarchive(fileUploaded.getUploadId(), is))
-                .doOnError(error -> logger.error("Unarchive failed for id" + fileUploaded.getUploadId(), error))
-                .subscribe(unarchived -> logger.info("Unarchived files: " + Arrays.toString(unarchived.toArray()) + "for upload id" + fileUploaded.getUploadId()));
+                .doOnError(error -> log.error("Unarchive failed for id" + fileUploaded.getUploadId(), error))
+                .subscribe(unarchived -> log.info("Unarchived files: " + Arrays.toString(unarchived.toArray()) + "for upload id" + fileUploaded.getUploadId()));
 
     }
 
--- src/main/java/ai/datahunters/md/server/photos/indexing/upload/UploadService.java
+++ src/main/java/ai/datahunters/md/server/photos/indexing/upload/UploadService.java
@@ -2,8 +2,7 @@ package ai.datahunters.md.server.photos.indexing.upload;
 
 import ai.datahunters.md.server.photos.indexing.filesystem.FileService;
 import ai.datahunters.md.server.photos.indexing.uploadid.UploadId;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.buffer.DataBufferUtils;
 import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.stereotype.Component;
@@ -16,23 +15,23 @@ import java.nio.file.StandardOpenOption;
 import java.util.stream.Collectors;
 
 @Component
+@Slf4j
 public class UploadService {
     public UploadService(FileService fileService) {
         this.fileService = fileService;
     }
 
     private FileService fileService;
-    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     public Mono<FileUploaded> handleUpload(UploadId uploadId, FilePart filePart) {
-        logger.info("Starting upload for id" + uploadId);
+        log.info("Starting upload for id" + uploadId);
         try {
             Path tempFile = fileService.createFileForUpload(uploadId);
 
             AsynchronousFileChannel channel = AsynchronousFileChannel.open(tempFile, StandardOpenOption.WRITE);
 
             return DataBufferUtils.write(filePart.content(), channel, 0)
-                    .doOnComplete(() -> logger.info("Upload " + uploadId + " completed"))
+                    .doOnComplete(() -> log.info("Upload " + uploadId + " completed"))
                     .collect(Collectors.counting())
                     .map(count -> new FileUploaded(uploadId, tempFile));
         } catch (IOException e) {
