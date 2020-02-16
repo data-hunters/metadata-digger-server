5c51f14c38896757a759ffffc0211621540a73f2
Michał Gutowski <michal@cucumbersome.net>
2020-02-16 14:54:15 +0100

    Hashes added

--- src/main/java/ai/datahunters/md/server/photos/search/json/Photo.java
+++ src/main/java/ai/datahunters/md/server/photos/search/json/Photo.java
@@ -1,12 +1,15 @@
 package ai.datahunters.md.server.photos.search.json;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Value;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @Value
+@JsonInclude(JsonInclude.Include.NON_ABSENT)
 public class Photo {
     private String id;
     @JsonProperty("base_path")
@@ -23,4 +26,25 @@ public class Photo {
     private List<String> labels;
     @JsonProperty("meta_data")
     private Map<String, List<String>> metaData;
+
+    @JsonProperty("hash_crc32")
+    private Optional<String> hashCrc32;
+
+    @JsonProperty("hash_md5")
+    private Optional<String> hashMd5;
+
+    @JsonProperty("hash_sha1")
+    private Optional<String> hashSha1;
+
+    @JsonProperty("hash_sha224")
+    private Optional<String> hashSha224;
+
+    @JsonProperty("hash_sha256")
+    private Optional<String> hashSha256;
+
+    @JsonProperty("hash_sha384")
+    private Optional<String> hashSha284;
+
+    @JsonProperty("hash_sha512")
+    private Optional<String> hashSha512;
 }
--- src/main/java/ai/datahunters/md/server/photos/search/solr/PhotoEntity.java
+++ src/main/java/ai/datahunters/md/server/photos/search/solr/PhotoEntity.java
@@ -10,6 +10,7 @@ import org.springframework.data.solr.core.mapping.SolrDocument;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @Value
 @SolrDocument(collection = "metadata_digger")
@@ -17,18 +18,46 @@ import java.util.Map;
 public class PhotoEntity {
     private @Id
     String id;
+
     private @Indexed(name = "base_path")
     String basePath;
+
     private @Indexed(name = "file_path")
     String filePath;
+
     private @Indexed(name = "file_type")
     String fileType;
+
     private @Indexed(name = "directory_names")
     List<String> directoryNames;
+
     private @Indexed(name = "tag_names")
     List<String> tagNames;
+
     private @Indexed(name = "labels")
     List<String> labels;
+
+    private @Indexed(name = "hash_crc32")
+    Optional<String> hashCrc32 = Optional.empty();
+
+    private @Indexed(name = "hash_md5")
+    Optional<String> hashMd5 = Optional.empty();
+
+    private @Indexed(name = "hash_sha1")
+    Optional<String> hashSha1 = Optional.empty();
+
+    private @Indexed(name = "hash_sha224")
+    Optional<String> hashSha224 = Optional.empty();
+
+    private @Indexed(name = "hash_sha256")
+    Optional<String> hashSha256 = Optional.empty();
+
+    private @Indexed(name = "hash_sha384")
+    Optional<String> hashSha284 = Optional.empty();
+
+    private @Indexed(name = "hash_sha512")
+    Optional<String> hashSha512 = Optional.empty();
+
     private @Dynamic
     @Field("md_*")
     Map<String, List<String>> metaData;
