b31db3847f4ad41c19b6e5e2c6437e31eb60c389
Michał Gutowski <michal@cucumbersome.net>
2020-04-18 18:53:45 +0200

    java version-removed

--- src/main/java/ai/datahunters/md/server/photos/indexing/IndexingEvent.java
+++
@@ -1,7 +0,0 @@
-package ai.datahunters.md.server.photos.indexing;
-
-import ai.datahunters.md.server.photos.indexing.uploadid.IndexingJobId;
-
-public interface IndexingEvent {
-    IndexingJobId getIndexingJobId();
-}
